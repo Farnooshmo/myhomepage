e0301eead494cf67d53e6bb6b2a406cdad68e7d0
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2022-09-28 16:48:54 +0100

    add closemenu JS

--- script.js
+++ script.js
@@ -67,12 +67,17 @@ test("There are at least 500 words on the page", () => {
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu(){
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-
+  
+  
+  const navLink = document.querySelectorAll(".nav-link");
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+    
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
